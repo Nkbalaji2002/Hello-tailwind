ddefce7f4d61db3b1899c0eebf77ab98ac06ce54
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-28 19:16:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="flex items-center h-screen justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello World!!!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
